09d79c6b6309301da3ce08501ec614ff9f317d5e
DC4-URBE-SOLE <88694392+DC4-URBE-SOLE@users.noreply.github.com>
2025-10-14 22:30:52 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,200 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dc4_modelo1_sala_360_03",
+      "name": "DC4_MODELO1_SALA_360_03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 2.956057016370993,
+        "pitch": 0.19443827608512798,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dc4_modelo1_sala_360_02",
+      "name": "DC4_MODELO1_SALA_360_02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -0.9151595990891899,
+        "pitch": 0.18615181330309838,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dc4_modelo1_sala_360_01",
+      "name": "DC4_MODELO1_SALA_360_01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dc4_modelo1_cozinha_360",
+      "name": "DC4_MODELO1_COZINHA_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -3.040546519387142,
+        "pitch": 0.05309255869313212,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-dc4_modelo1_banheiro_360",
+      "name": "DC4_MODELO1_BANHEIRO_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 0.1138258207822318,
+        "pitch": -0.02991725570942272,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dc4_modelo1_quarto1_360",
+      "name": "DC4_MODELO1_QUARTO1_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -1.4710073426076171,
+        "pitch": 0.3599133964137753,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-dc4_modelo1_quarto2_360",
+      "name": "DC4_MODELO1_QUARTO2_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -3.0636817386670323,
+        "pitch": 0.5464596804080308,
+        "fov": 1.1678810529903434
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
